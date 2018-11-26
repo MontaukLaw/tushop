134b8883c978d435d8142dd1162c47bf740867a6
Marc <zunly@hotmail.com>
2018-11-26 19:08:34 +0800

    基本搞定了分页, 剩下一个问题是如何把分页的前台部分嵌入bootstrap样式,不过这个问题倒是不怎么重要

--- application/index/controller/AdminLogController.php
+++ application/index/controller/AdminLogController.php
@@ -16,13 +16,26 @@ use think\Db;
 class AdminLogController extends Controller
 {
 
-    public function viewTest()
+    public function viewTest($page)
     {
         //$adminLogs = AdminModel::get('1=1', 'admin');
         //$adminLogs = AdminModel::all(null, 'admin');
         //$adminLogs = Db::table('ty_admin_log')->where('log_id', '>', 0)->limit(0, 10)->select();
-        $adminLogs=AdminModel::getAllAdminLogWithAdminName();
-        return view()->assign('list', $adminLogs);
+        //$adminLogs = AdminModel::getAllAdminLogWithAdminName();
+        //return view()->assign('list', $adminLogs);
+
+
+        // 查询状态为1的用户数据 并且每页显示10条数据 总记录数为1000
+        $adminLogs = AdminModel::getAllAdminLogWithAdminName($page);
+        //$list = User::where('status', 1)->paginate(10, 1000);
+        // 获取分页显示
+        $page = $adminLogs->render();
+        // 模板变量赋值
+        $this->assign('list', $adminLogs);
+        $this->assign('page', $page);
+        // 渲染模板输出
+        return $this->fetch();
+
     }
 
     public function getAllAdminLog()
@@ -38,11 +51,32 @@ class AdminLogController extends Controller
         //$adminLogs = Db::table('ty_admin_log')->where('log_id', '>', 0)->order('log_time','desc')->limit(0, 10)->select();
 
         //$adminLogs = AdminModel::all(null, 'admin');
-        $adminLogs=AdminModel::getAllAdminLogWithAdminName();
+        $adminLogs = AdminModel::getAllAdminLogWithAdminName();
 
         return json($adminLogs);
     }
 
+    public function initAdminLog()
+    {
+        $adminLog = new AdminModel();
 
+        dump($adminLog);
+    }
+
+    public function getAllAdminLogWithAdminPaged()
+    {
+
+        // 查询状态为1的用户数据 并且每页显示10条数据 总记录数为1000
+        $adminLogs = AdminModel::getAllAdminLogWithAdminName()->paginate(10, 1000);;
+        //$list = User::where('status', 1)->paginate(10, 1000);
+        // 获取分页显示
+        $page = $adminLogs->render();
+        // 模板变量赋值
+        $this->assign('list', $adminLogs);
+        $this->assign('page', $page);
+        // 渲染模板输出
+        return $this->fetch();
+
+    }
 
 }
