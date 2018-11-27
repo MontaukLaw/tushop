d3d69259db229dd8793b92ce18e8666c910a9cc5
Marc <zunly@hotmail.com>
2018-11-27 10:40:54 +0800

    验证码是用ajax实现校验的, 由于异步机制, 验证码先验证, 结果却会稍微晚一点返回, 所以js的debug也是很重要的啊, 接下来是验证码点击刷新

--- application/index/controller/CaptchaController.php
+++ application/index/controller/CaptchaController.php
@@ -8,7 +8,7 @@
 
 namespace app\index\controller;
 
-
+use app\util\JsonMsg;
 use think\Controller;
 
 class CaptchaController extends Controller
@@ -20,13 +20,18 @@ class CaptchaController extends Controller
     }
 
     // 验证码检测
-    public function check($code='')
+    public function check($code = '')
     {
+        //准备输出用的msg
+        $jmsg = new JsonMsg;
         $captcha = new \think\captcha\Captcha();
         if (!$captcha->check($code)) {
-            $this->error('验证码错误');
+            $jmsg->setSuccess(false);
+            $jmsg->setMsg('验证码错误');
         } else {
-            $this->success('验证码正确');
+            $jmsg->setSuccess(true);
         }
+
+        return json($jmsg);
     }
 }
