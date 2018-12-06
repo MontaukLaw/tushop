530d6fa3ac3c291b58ddae02959d5cd9aa06f60e
Marc <zunly@hotmail.com>
2018-12-06 18:32:25 +0800

    move back

---
+++ application/index/controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zunly
+ * Date: 2018/11/27
+ * Time: 12:34
+ */
+
+namespace app\index\controller;
+
+
+class UserController
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+}
